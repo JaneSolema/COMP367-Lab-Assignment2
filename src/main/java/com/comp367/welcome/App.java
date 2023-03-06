70cbf8e2fd75a2f760f6ea7cedfab3ab1e08b9d3
JaneSolema <105816842+JaneSolema@users.noreply.github.com>
2023-03-05 19:40:50 -0500

    modify maven app

--- src/main/java/com/comp367/welcome/App.java
+++ src/main/java/com/comp367/welcome/App.java
@@ -1,7 +1,18 @@
 package com.comp367.welcome;
+import java.time.LocalTime;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Welcome to COMP367");
+	public static void main(String[] args) {
+        String name = "John"; // replace with your own name or use user input
+        LocalTime now = LocalTime.now();
+        String message;
+        
+        if (now.isBefore(LocalTime.NOON)) {
+            message = "Good morning, " + name + ", Welcome to COMP367";
+        } else {
+            message = "Good afternoon, " + name + ", Welcome to COMP367";
+        }
+        
+        System.out.println(message);
     }
 }
